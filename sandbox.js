5ccb5a80c4f22b90aafcd1811cc6ebd664529dc4
tunny17 <tvnji17@gmail.com>
2022-10-29 21:55:45 +0100

    media and javascript fix

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,11 @@
-const rangeInputs = document.querySelectorAll('input[type="range"]')
+const rangeInputs = document.querySelectorAll('input[type="range"]');
+let logoContainer = document.getElementById('logoContainer');
+let navOne = document.getElementById('nav1');
+let navTwo = document.getElementById('nav2');
+let menu = document.getElementById('menu');
+
+
+
 
 function handleInputChange(e) {
   let target = e.target
@@ -16,4 +23,27 @@ rangeInputs.forEach(input => {
     input.addEventListener('input', handleInputChange)
 })
   
-numberInput.addEventListener('input', handleInputChange)
+
+
+
+menu.addEventListener('click', changeNav);
+
+function changeNav() {
+  let menuSrc = document.getElementById('menu').src;
+  if(menuSrc.indexOf('/img/home/hammenu.svg')!=-1) {
+    document.getElementById('menu').src = '/img/home/icon-close.svg';
+  } else {
+    document.getElementById('menu').src = '/img/home/hammenu.svg';
+  }
+  // let buttonSrc = document.getElementById("menu").src;
+  // if (buttonSrc.indexOf('/img/menu.svg')!=-1) {
+  //     document.getElementById("menu").src = '/img/icon-close.svg'
+  // } else {
+  //     document.getElementById("menu").src ='/img/menu.svg'
+  // }
+  console.log('yam');
+  logoContainer.classList.toggle('background-color');
+  navOne.classList.toggle('visible');
+  navTwo.classList.toggle('visible');
+
+}
